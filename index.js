b0710d011128cc10c24f2f49e4b4f5381fa061e2
Irfan <irfansk.se@gmail.com>
2020-10-14 20:57:47 +0530

    Update UI

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
+var sound = true;
 var count = document.querySelectorAll(".drum").length;
+
+//Mute Sound
+document.querySelector(".mute").addEventListener("click", function () {
+    if (sound) {
+        sound = false;
+        document.getElementById("volume-icon").classList.remove("fa-volume-up");
+        document.getElementById("volume-icon").classList.add("fa-volume-mute");
+    } else {
+        sound = true;
+        document.getElementById("volume-icon").classList.remove("fa-volume-mute");
+        document.getElementById("volume-icon").classList.add("fa-volume-up");
+    }
+});
 
 //Detecting Button Press
 for (var i = 0; i < count; i++) {
-
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
         let buttonInnerHTML = this.innerHTML;
         makeSound(buttonInnerHTML);
@@ -18,33 +31,70 @@ document.addEventListener("keydown", function (event) {
 
 function makeSound(key) {
 
+    let display = document.querySelector(".display");
+
     switch (key) {
-        case 'w': let tom1 = new Audio("sounds/tom-1.mp3");
-            tom1.play();
+        case 'a': let heater1 = new Audio("https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3");
+            display.innerHTML = "Heater 1";
+            if (sound === true) {
+                heater1.play();
+            }
             break;
 
-        case 'a': let tom2 = new Audio("sounds/tom-2.mp3");
-            tom2.play();
+        case 's': let heater2 = new Audio("https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3");
+            display.innerHTML = "Heater 2";
+            if (sound === true) {
+                heater2.play();
+            }
             break;
 
-        case 's': let tom3 = new Audio("sounds/tom-3.mp3");
-            tom3.play();
+        case 'd': let heater3 = new Audio("https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3");
+            display.innerHTML = "Heater 3";
+            if (sound === true) {
+                heater3.play();
+            }
             break;
 
-        case 'd': let tom4 = new Audio("sounds/tom-4.mp3");
-            tom4.play();
+        case 'f': let heater4 = new Audio("https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3");
+            display.innerHTML = "Heater 4";
+            if (sound === true) {
+                heater4.play();
+            }
             break;
 
-        case 'j': let crash = new Audio("sounds/crash.mp3");
-            crash.play();
+        case 'g': let clap = new Audio("https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3");
+            display.innerHTML = "Clap 4";
+            if (sound === true) {
+                clap.play();
+            }
             break;
 
-        case 'k': let snare = new Audio("sounds/snare.mp3");
-            snare.play();
+        case 'h': let openHH = new Audio("https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3");
+            display.innerHTML = "Open-HH";
+            if (sound === true) {
+                openHH.play();
+            }
             break;
 
-        case 'l': let kick = new Audio("sounds/kick-bass.mp3");
-            kick.play();
+        case 'j': let kickNHat = new Audio("https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3");
+            display.innerHTML = "Kick-n'-Hat";
+            if (sound === true) {
+                kickNHat.play();
+            }
+            break;
+
+        case 'k': let kick = new Audio("https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3");
+            display.innerHTML = "Kick";
+            if (sound === true) {
+                kick.play();
+            }
+            break;
+
+        case 'l': let closedHH = new Audio("https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3");
+            display.innerHTML = "Closed-HH";
+            if (sound === true) {
+                closedHH.play();
+            }
             break;
 
         default: console.log("Oops!, This button doesn't work.");
