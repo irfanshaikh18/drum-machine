d41928ca4998f331705442807ffd44ad044cbfb1
Irfan Shaikh <irfansk.se@gmail.com>
2020-11-04 16:35:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -36,63 +36,63 @@ function makeSound(key) {
     switch (key) {
         case 'a': let heater1 = new Audio("https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3");
             display.innerHTML = "Heater 1";
-            if (sound === true) {
+            if (sound) {
                 heater1.play();
             }
             break;
 
         case 's': let heater2 = new Audio("https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3");
             display.innerHTML = "Heater 2";
-            if (sound === true) {
+            if (sound) {
                 heater2.play();
             }
             break;
 
         case 'd': let heater3 = new Audio("https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3");
             display.innerHTML = "Heater 3";
-            if (sound === true) {
+            if (sound) {
                 heater3.play();
             }
             break;
 
         case 'f': let heater4 = new Audio("https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3");
             display.innerHTML = "Heater 4";
-            if (sound === true) {
+            if (sound) {
                 heater4.play();
             }
             break;
 
         case 'g': let clap = new Audio("https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3");
             display.innerHTML = "Clap 4";
-            if (sound === true) {
+            if (sound) {
                 clap.play();
             }
             break;
 
         case 'h': let openHH = new Audio("https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3");
             display.innerHTML = "Open-HH";
-            if (sound === true) {
+            if (sound) {
                 openHH.play();
             }
             break;
 
         case 'j': let kickNHat = new Audio("https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3");
             display.innerHTML = "Kick-n'-Hat";
-            if (sound === true) {
+            if (sound) {
                 kickNHat.play();
             }
             break;
 
         case 'k': let kick = new Audio("https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3");
             display.innerHTML = "Kick";
-            if (sound === true) {
+            if (sound) {
                 kick.play();
             }
             break;
 
         case 'l': let closedHH = new Audio("https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3");
             display.innerHTML = "Closed-HH";
-            if (sound === true) {
+            if (sound) {
                 closedHH.play();
             }
             break;
